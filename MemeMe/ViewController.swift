f9e3dacda459398c5a99c692cb090b4cb9ac8900
Kenan Ozdamar <kenan.ozdamar@gmail.com>
2016-05-13 13:41:38 -0400

    - Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Kenan Ozdamar on 5/13/16.
+//  Copyright © 2016 OKO Baycity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
