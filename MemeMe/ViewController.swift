78dda06dc7b1a1c90633d62f98e61fc4cc2f9d6a
Kenan Ozdamar <kenan.ozdamar@gmail.com>
2016-05-16 13:04:50 -0400

    - Added ImageView to display selected image/camera image in maun view controller.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    //UIImagePickerControllerDelegate protocol defines functionality for how image selected from ImagePicker should be processed.
+    //UINavigationControllerDelegate needed to set this class as the protocol fot the delegate.
+    
+    //outlet to the image being displaued in main view conttoller
+    @IBOutlet weak var image_view: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +26,38 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func pickImageFromUIImagePicker(sender: AnyObject) {
+        //when user select "album" button this method is triggered which displays an ios include view controller
+        //that allows user to pick image from album.
+
+        print("pickImageFromUIImagePicker has been executed.")
+        
+        //load an instance of the ios provided view controller to pick image from library
+        let imagePickerController = UIImagePickerController()
+        
+        //set the delegate from the UIImagePicker. this class is defining the protocol.
+        imagePickerController.delegate = self
+        
+        //present the view controller.
+        self.presentViewController(imagePickerController, animated: true, completion: nil)
+
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]){
+        //override this method from UIImagePickerControllerDelegate protocol to specify how selected image should be diplayed.
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            image_view.image = image
+            print("setting image")
+        }else {
+            print("something has gone wrong picking the image.")
+        }
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController){
+        
+    }
 }
 
